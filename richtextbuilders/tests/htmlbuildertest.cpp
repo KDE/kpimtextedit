209c82e578dfdff02b154dacf7699b688ffcbb09
Stephen Kelly <steveire@gmail.com>
2008-09-17 11:55:46 +0000

    Create the first unit test for the html builder. This will be expanded later.
    svn path=/trunk/playground/pim/richtextbuilders/; revision=861881

---
+++ richtextbuilders/tests/htmlbuildertest.cpp
@@ -0,0 +1,36 @@
+#include <QtTest>
+#include <QtCore>
+#include <qtest_kde.h>
+#include <qtestevent.h>
+#include <QRegExp>
+#include <QTextDocument>
+
+#include <kdebug.h>
+#include "../markupdirector.h"
+#include "../htmlbuilder.h"
+
+class TestHtmlOutput: public QObject
+{
+    Q_OBJECT
+private slots:
+    void testSingleFormat();
+};
+
+void TestHtmlOutput::testSingleFormat()
+{
+    QTextDocument *doc = new QTextDocument();
+    doc->setHtml("This <b>text</b> is bold.");
+
+    HTMLBuilder *hb = new HTMLBuilder();
+    MarkupDirector *md = new MarkupDirector(hb);
+    md->constructContent(doc);
+    QString result = hb->getResult();
+
+    QRegExp regex(QString("^<p style=\"margin-top:0;margin-bottom:0;\">\\s*This <strong>text</strong> is bold.\\s*</p>\\s*$"));
+
+    QVERIFY(regex.exactMatch(result));
+    
+}
+
+QTEST_KDEMAIN(TestHtmlOutput, GUI)
+#include "htmlbuildertest.moc"
