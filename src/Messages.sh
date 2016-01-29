92709fe32193d2e177e8648c8b2d28e005c795e8
Frederik Schwarzer <schwarzer@kde.org>
2016-01-29 11:11:36 +0100

    Do not extract mesages from tests/ folders.

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-$XGETTEXT `find . -name "*.cpp" -o -name "*.h"` -o $podir/libkpimtextedit.pot
+$XGETTEXT `find . -name "*.cpp" -o -name "*.h" | grep -v '/tests/'` -o $podir/libkpimtextedit.pot
