6ccc2ae6a5c2f3c1ef15ff57d6a67353bc2be09c
Montel Laurent <montel@kde.org>
2017-04-03 13:23:54 +0200

    Remove spell checking

--- src/inserthtmldialog.cpp
+++ src/inserthtmldialog.cpp
@@ -44,6 +44,7 @@ public:
         QLabel *label = new QLabel(i18n("Insert HTML tags and texts:"));
         lay->addWidget(label);
         editor = new InsertHtmlEditor;
+        editor->setSpellCheckingSupport(false);
         KPIMTextEdit::PlainTextEditorWidget *editorWidget = new KPIMTextEdit::PlainTextEditorWidget(editor);
         lay->addWidget(editorWidget);
         label = new QLabel(i18n("Example: <i> Hello word </i>"));
