a43ff94cca59715d5221a776f00650d2b1ea8e7c
Laurent Montel <montel@kde.org>
2021-01-01 14:56:12 +0100

    GIT_SILENT: happy new year

--- src/grantleebuilder/texthtmlbuilder.cpp
+++ src/grantleebuilder/texthtmlbuilder.cpp
@@ -1,5 +1,5 @@
 /*
-   SPDX-FileCopyrightText: 2020 Laurent Montel <montel@kde.org>
+   SPDX-FileCopyrightText: 2020-2021 Laurent Montel <montel@kde.org>
    based on code from Stephen Kelly <steveire@gmail.com>
 
    SPDX-License-Identifier: LGPL-2.0-or-later
