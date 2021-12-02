f50620510b284df4af4e560ee212951106bc2295
Laurent Montel <montel@kde.org>
2021-12-02 13:57:48 +0100

    GIT_SILENT: activate reuse lint CI support

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,2 +1,5 @@
 #! /bin/sh
+# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-FileCopyrightText: none
+
 $XGETTEXT `find . -name "*.cpp" -o -name "*.h" | grep -v '/tests/'` -o $podir/libkpimtextedit.pot
