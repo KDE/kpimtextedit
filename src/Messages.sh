5ee3555252035ce303992a8caecb871e5b4ebde0
Laurent Montel <montel@kde.org>
2022-11-19 17:14:56 +0100

    Move autotests/tests in own directory

--- src/Messages.sh
+++ src/Messages.sh
@@ -2,4 +2,4 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # SPDX-FileCopyrightText: none
 
-$XGETTEXT `find . -name "*.cpp" -o -name "*.h" | grep -v '/tests/'` -o $podir/libkpimtextedit.pot
+$XGETTEXT `find . -name "*.cpp" -o -name "*.h" | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/libkpimtextedit.pot
